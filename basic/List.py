c1de13020371d8273f18dec6efa1a788b2c4344e
Arabinda Moni <arabindamoni@gmail.com>
2020-05-29 20:41:25 +0530

    day 4 commit 2

---
+++ basic/List.py
@@ -0,0 +1,65 @@
+### 
+# range()
+###
+
+# 1.  create a list of integers from 0 to n
+# l = list(range(10))
+# print(l)
+
+# # 2.  create a list of integers from 3 to 10
+# l2 = list(range(3,10))
+# print(l2)
+
+# # 3.  create a list of integers from 0, 10 with step size of 2
+# # 0, 2, 4, ... 
+# l3 = list(range(0,10,2))
+# print(l3)
+
+# # 4. create a list of integers from 10 to 1
+# l4 = list(range(10,0,-1))
+# print(l4)
+
+
+# create a list of integers from 0, n with step size of k
+# l = list(range(0, n, k))
+
+# create a list of integers from 10 to 0
+# l5 = list(range(10,-1,-1))
+
+# range(start_position=0, end_position, step_size=1)
+# by default 
+
+
+#############################
+##############################
+
+
+
+
+l = list(range(10))
+# print(l)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# # [0, 1, 2, 3, 4]
+# # [5, 6, 7, 8, 9]
+# print(l[:5])
+# print(l[5:])
+
+# # reverse the list [4,3,2,1,0] -> [4,3,2,1]
+# print(l[-1:-11:-1])
+
+# print [0,1,8,9]
+
+# l = [1,2,3]
+# l2 = [5,4,7]
+
+# print(l+l2)
+# l.extend(l2)
+# print(l)
+
+# # print(l[:2]+l[9:])
+# l.append(10)
+# l.pop(6)
+# l.insert(0,50)
+
+# https://developers.google.com/edu/python
