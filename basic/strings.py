6dc8e55f953b3f77ba7ed3b64b7b1dcd453362d6
Arabinda Moni <arabindamoni@gmail.com>
2020-05-31 20:02:32 +0530

    day 5

--- basic/strings.py
+++ basic/strings.py
@@ -41,15 +41,56 @@ s = "hello munai kemon achis"
 # k = s.index(' ',j+1)
 
 # print(s[k+1:]+ " "+ s[j+1:k] + " " + s[i+1:j]+" "+ s[0:i])
-res = []
-i = 0
-for k in range(3):
-    j = s.index(" ",i)
-    r = s[i:j]
-    i = j + 1
-    res.append(r)
-res.append(s[i:])
+# res = []
+# i = 0
+# for k in range(3):
+#     j = s.index(" ",i)
+#     r = s[i:j]
+#     i = j + 1
+#     res.append(r)
+# res.append(s[i:])
 
-print(res)
-print(" ".join(res[::-1]))
+# join
+# print(res)
+# print(" ".join(res[::-1]))
 
+# split
+s = "hello munai kemon achis"
+# # output = "achis kemon munai hello"
+# # print(s.split())
+# # print("my run")
+# def rev(s):
+#     p = s.split()
+#     #print(p[::-1])
+#     return " ".join(p[::-1])
+
+# print(rev("abc def ghi"))
+
+# print(s.upper())
+
+# replace
+# s = "hello munai kemon achis"
+# s = "hello,munai,kemon,achis"
+# print(s.replace(' ',','))
+
+
+# l = [4,5,2,4,0,22,1]
+# l.sort(reverse=True)
+# print(l)
+
+# l2 = sorted(l)
+# print(l2)
+
+# Tuple
+# l = [(0,0),(1,0),(2,50),(8,3),(3,6)]
+# # sort based on x/y coordinate
+# # print(l[0][1])
+# l2 = sorted(l, key = lambda item:item[1])
+# print(l2)
+
+# order of sort in tuple
+l = [("one",1),("two",2),("one",2)]
+#[('one', 1), ('two', 2), ('one', 2)]
+#output: [('one', 1),  ('one', 2), ('two', 2)]
+l1 = sorted(l, key = lambda item:(item[1], item[0]))
+print (l1)
