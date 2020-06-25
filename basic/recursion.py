61cf4412eeeaf44117cf06d7345efc62bd51bbef
Arabinda Moni <arabindamoni@gmail.com>
2020-06-25 19:32:02 +0530

    subset generation

--- basic/recursion.py
+++ basic/recursion.py
@@ -270,17 +270,79 @@ def generate_subset_size_3(s, n):
     return res 
 
 # l = [[2,3],[5,6],[4,5,6,7,8]] 
-# res = [[1,2,3],[1,5,6],[1,4,5,6,7,8]]
-def append_1(l):
+# # res = [[1,2,3],[1,5,6],[1,4,5,6,7,8]]
+# def append_1(l):
+#     res = []
+#     return res
+
+# # l = [3,4,5]
+# # res = [1,3,4,5]
+# def append_1_easy(l):
+#     res = []
+#     res.append(1)
+    
+#     res[] =1
+#     return res 
+
+
+# s = [1,2,3] = [[],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
+# s1 = [2,3] = [[],[2],[3],[2,3]]   = [[],[2],[3],[2,3],[1],[1,2],[1,3],[1,2,3]]
+# s2 = [3] = [[],[3]] = [[],[3],[2],[2,3]]
+
+def generate_subsets(s):
     res = []
+    res = generate_subsets(s[0])
+    return res 
+
+#  n = 2
+# # [[1,2],[1,3][1,4],[1,5],[2,3],[2,4][2,5],[3,4],[3,5],[4,5]] 
+# # list(range(2,5)) => [2,3,4]
+def generate_subset_size_2(s):
+    res = []
+    for i in range(0,len(s)): # 1
+        for j in range(i+1,len(s)):
+            res.append([s[i],s[j]])
+            
     return res
+    
+# s = [1,2,3,4,5]
+# print(generate_subset_size_2(s))
+
+# s = [2,3,4,5] n=2  [[2,3],[2,4],[2,5],[3,4],[3,5],[4,5]]
+# n=3 => [[1,2,3],[1,2,4],[1,2,5],[1,3,4],[1,3,5],[1,4,5],[2,3,4],[2,3,5],[2,4,5],[3,4,5]]
+def generate_subset_size_3(s):
+    res = []
+    res1 = generate_subset_size_2(s)
+    for i in range(0,len(s)-2):        
+        res = generate_subset_size_2(s[i+1:]) #[2,3]
+        for j in range(len(res)):
+            res1.append([i+1] + res[j])
+    # print (res1)
+    # res1.append(generate_subset_size_2(s))
+    return res1 
+s = [1,2,3,4,5]
+print (generate_subset_size_3(s))
+
+# l = [[2,3],[5,6],[4,5,6,7,8]] 
+# # res = [[1,2,3],[1,5,6],[1,4,5,6,7,8]]
+# def append_1(l):
+#     res = []
+    
+#     for i in range(len(l)):
+        
+#         res.append([1] + l[i])
+#     return res 
+
+# print(append_1(l))
+
 
 # l = [3,4,5]
-# res = [1,3,4,5]
-def append_1_easy(l):
-    res = []
-    res.append(1)
-    
-    res[] =1
-    return res 
+# # # res = [1,3,4,5]
+# def append_1_easy(l):
+#     res = []
+#     res.append(1)
+#     res = res + l
+#     return res 
+
+# print(append_1_easy(l))
 
